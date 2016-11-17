77edc5c34e8e5f35d8fcdca31ad8f40063b15444
Cal Rueb <calvin.c.rueb@gmail.com>
2016-11-17 15:10:33 -0600

    Added framework for watchlist

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ running = True;
 print("Welcome to the Rueb Stock Trader\nType the corresponding number to do a command or type 'exit' to exit")
 
 while(running):
-    print("\n1) Look up price\n2)View watchlist")
+    print("\n1) Look up price\n2)Watchlist options")
     print(">", end=" ")
     user_input = input()
     
@@ -18,7 +18,16 @@ while(running):
     
     #watch list
     elif(user_input == "2"):
-        print("Watchlist")
+        print("1) View watchlist\n2) Add stock to watchlist\n3) Remove stock remove watchlist")
+        print(">", end=" ")
+        user_input = input()
+        
+        if(user_input == "1"):
+            print("Unimplemented")
+        elif(user_input == "2"):
+            print("Unimplemented")
+        elif(user_input == "3"):
+            print("Unimplemented")
     
     #exit
     elif(user_input == "exit"):
