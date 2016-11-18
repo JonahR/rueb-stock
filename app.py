d049abd5fb4529dc90daf82641a567b2c30a45d8
Cal Rueb <calvin.c.rueb@gmail.com>
2016-11-18 15:50:50 -0600

    Split up app.py code

--- app.py
+++ app.py
@@ -1,41 +1,74 @@
 from stock import Stock
 
-running = True;
-
-print("\n\n\nWelcome to the Rueb Stock Trader\nType the corresponding number to do a command or type 'exit' to exit")
-
-while(running):
-    print("\n1) Look up price\n2) Watchlist options")
+#Methods for each available command
+def stockPriceCmd():
+    print("Stock price for:", end=" ")
+    ticker = input()
+    lookedup_stock = Stock(ticker)
+    print(lookedup_stock.getPrice())
+    
+def watchlistCmd():
+    print("1) View watchlist\n2) Add stock to watchlist\n3) Remove stock from watchlist")
     print(">", end=" ")
     user_input = input()
-    
-    #Look up price
+        
     if(user_input == "1"):
-        print("Stock price for:", end=" ")
-        ticker = input()
-        lookedup_stock = Stock(ticker)
-        print(lookedup_stock.getPrice())
-    
-    #watch list
+        print("Unimplemented")
     elif(user_input == "2"):
-        print("1) View watchlist\n2) Add stock to watchlist\n3) Remove stock from watchlist")
+        print("Unimplemented")
+    elif(user_input == "3"):
+        print("Unimplemented")
+
+def portfolioCmd():
+    print("1) Portfolio details\n2) Buy stock\n3) Sell stock")
+    print(">", end=" ")
+    user_input = input()
+        
+    if(user_input == "1"):
+        print("Unimplemented")
+    elif(user_input == "2"):
+        print("Unimplemented")
+    elif(user_input == "3"):
+        print("Unimplemented")
+        
+#Exits application
+#Changes global variable running
+#Potientially add code for saving/backing up data here
+def exit():
+    global running
+    running = False
+    print("Goodbye")
+    
+#starts application and creates loop until exit command
+def start():
+    global running
+    running = True
+    print("\n\n\nWelcome to the Rueb Stock Trader\nType the corresponding number to do a command or type 'exit' to exit")
+
+    while(running):
+        print("\n1) Look up price\n2) Watchlist options")
         print(">", end=" ")
         user_input = input()
-        
-        if(user_input == "1"):
-            print("Unimplemented")
-        elif(user_input == "2"):
-            print("Unimplemented")
-        elif(user_input == "3"):
-            print("Unimplemented")
     
-    #exit
-    elif(user_input == "exit"):
-        running = False;
-        print("Goodbye")
-   
-   #bad input
-    else:
-        print("That is an invalid command.")
+        #Look up price
+        if(user_input == "1"):
+            stockPriceCmd()
+    
+        #watch list
+        elif(user_input == "2"):
+            watchlistCmd()
+    
+        #portfolio
+        elif(user_input == "3"):
+            portfolioCmd()
         
+        #exit
+        elif(user_input == "exit"):
+            exit()
         
+        #bad input
+        else:
+            print("That is an invalid command.")
+            
+#start application
+start()
